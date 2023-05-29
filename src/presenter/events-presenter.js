3c1ff60676f2342a559763c8823a8e1e9158989f
Khudorozhkova <E.D.Khudorozhkova@urfu.me>
2023-05-30 00:25:50 +0500

    Revert "Революция или эволюция? (часть 2)"
    This reverts commit a56e9307e360054e8d7f8d2cb0fcf4e084078af1.

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -2,7 +2,6 @@ import PointsView from '../view/points-view.js';
 import PreviewPointView from '../view/preview-point-view.js';
 import EditingFormView from '../view/editing-form-view.js';
 import SortingView from '../view/sorting-view.js';
-import NoPointView from '../view/no-point-view.js';
 import { render } from '../render.js';
 
 export default class EventsPresenter {
@@ -24,18 +23,14 @@ export default class EventsPresenter {
     this.#destinations = [...this.#pointsModel.destinations];
     this.#offers = [...this.#pointsModel.offers];
 
-    if (this.#boardPoints.length === 0) {
-      render(new NoPointView(), this.#tripContainer);
+    render(new SortingView(), this.#tripContainer);
+    render(this.#pointsList, this.#tripContainer);
+
+    for (const point of this.#boardPoints){
+      this.#renderPoint(point);
     }
-    else {
-      render(new SortingView(), this.#tripContainer);
-      render(this.#pointsList, this.#tripContainer);
-      for (const point of this.#boardPoints){
-        this.#renderPoint(point);
-      }
-    }   
   }
-  
+
   #renderPoint = (point) => {
     const pointComponent = new PreviewPointView(point, this.#destinations, this.#offers);
     const pointEditComponent = new EditingFormView(point, this.#destinations, this.#offers);
